08dbb1af9014eff87a7bae16a5b55a77139c58ee
Paul Haerle <hello@phaer.org>
2020-04-05 18:20:31 +0200

    Bump ruby to 2.6.6. (#21)
    * Bump ruby to 2.6.6.
    
    * Bump ruby-build to v20200401
    
    * Fix ruby version in tests.

--- spec/debian/debian_spec.rb
+++ spec/debian/debian_spec.rb
@@ -20,7 +20,7 @@ describe 'Ansible Debian target' do
     end
 
     describe command('ruby -v') do
-      its(:stdout) { should match(/2\.6\.5/) }
+      its(:stdout) { should match(/2\.6\.6/) }
     end
 
     describe file('/usr/bin/nodejs') do
@@ -71,7 +71,7 @@ describe 'Ansible Debian target' do
     end
 
     describe command('ruby-build --version') do
-      its(:stdout) { should match(/ruby-build 20191004/) }
+      its(:stdout) { should match(/ruby-build 20200401/) }
     end
 
     describe file('/home/mastodon/live') do
